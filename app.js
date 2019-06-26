007104155e0d91b93268e2710bdf1e9a97d2dfa3
Jamie Green (Song Kran) <James@UserYouiMac4415.localdomain>
2019-06-26 16:18:11 -0700

    Adding a javascript file

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = prompt('What is your name?');
+alert('Welcome' + ' ' + name);
+
+var color = prompt('what color do you want to change this page to be?');
+
+document.body.page.style.background = Color;
+
+
+
