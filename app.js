94e78476a7aded36723c072756ba7f8eb8c4a52d
Jamie Green (Song Kran) <James@UserYouiMac4415.localdomain>
2019-06-27 16:11:17 -0700

    I think this works, but I'm not sure yet.

--- app.js
+++ app.js
@@ -13,13 +13,31 @@ function changeFont() {
     document.body.style.fontFamily = font;
 }
 
-function changeCSS(msg, cssProperty, val) {
-    var answer = prompt(msg);
-    document.body.style.cssProperty = val;
-    confirm('Are you sure? Really?');
+// function changeCSS(msg, cssProperty, val) {
+//     var answer = prompt(msg);
+//     document.body.style.cssProperty = val;
+//     confirm('Are you sure? Really?');
+    // getName();
+    // changePageColor();
+    // changeFont();
+    // changeCSS();
 }
 
 getName();
 changePageColor();
 changeFont();
-changeCSS();
+// changeCSS();
+
+    // array = [jogging, hunting, biking, killing, eating, laughing, living]
+    // while(array(laughing));
+
+function question() {
+    var answer = prompt('What is my favorite activity?' + 'Jogging, hunting, biking, killing, eating, laughing, or living?')
+    if (answer !== laughing) {
+    prompt('It is not' + ' ' + answer + '!')
+    else (answer == laughing) {
+    alert("Yeah! I love laughing!")
+    }
+}
+}
+question()
