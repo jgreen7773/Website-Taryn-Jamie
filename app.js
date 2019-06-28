896d513ff88986b4fb4660e9cfaa244c83f484a9
Jamie Green (Song Kran) <James@UserYouiMac4415.localdomain>
2019-06-27 18:55:48 -0700

    I got the loop to work but there is no display of html code OR css now.

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
-function getName() {
-    var name = prompt('what is your name?');
-    alert('welcome' + ' ' + name);
-}
+// function getName() {
+//     var name = prompt('what is your name?');
+//     alert('welcome' + ' ' + name);
+// }
 
-function changePageColor() {
-    var color = prompt('what color do you want this page to be?');
-    document.body.style.background = color;
-}
+// function changePageColor() {
+//     var color = prompt('what color do you want this page to be?');
+//     document.body.style.background = color;
+// }
 
-function changeFont() {
-    var font = prompt('what font family do you want to have?')
-    document.body.style.fontFamily = font;
-}
-
+// function changeFont() {
+//     var font = prompt('what font family do you want to have?');
+//     document.body.style.fontFamily = font;
+// }
+// getName();
+// changePageColor();
+// changeFont();
+// 
 // function changeCSS(msg, cssProperty, val) {
 //     var answer = prompt(msg);
 //     document.body.style.cssProperty = val;
 //     confirm('Are you sure? Really?');
+
     // getName();
     // changePageColor();
-    // changeFont();
-    // changeCSS();
-}
-
-getName();
-changePageColor();
-changeFont();
+//     changeFont();
+//     changeCSS();
+// }
 // changeCSS();
-
+// 
     // array = [jogging, hunting, biking, killing, eating, laughing, living]
     // while(array(laughing));
-
+// 
+// function question() {
+//     var answer = prompt('What is my favorite activity?' + 'Hunting, killing, laughing, or living?');
+//     if (answer !== "laughing") {
+//     prompt('It is not' + ' ' + answer + '!');}
+//     else {(answer == "laughing");
+//     break;
+//     alert("Yeah! I love laughing!");
+//     }
+// }
+// question();
+// 
+// function question() {
+//     var answer = prompt('What is my favorite activity?' + 'Hunting, killing, laughing, or living?')
+//     if (answer !== "laughing") {
+//     prompt('It is not' + ' ' + answer + '!')}
+//     else {(answer == "laughing")
+//     alert("Yeah! I love laughing!")
+//     }
+// }
+// question()
+// 
 function question() {
-    var answer = prompt('What is my favorite activity?' + 'Jogging, hunting, biking, killing, eating, laughing, or living?')
-    if (answer !== laughing) {
-    prompt('It is not' + ' ' + answer + '!')
-    else (answer == laughing) {
-    alert("Yeah! I love laughing!")
+    array = ['Hunting', 'killing', 'laughing', 'living']
+    var answer = prompt('What is my favorite activity?' + ' ' + 'Hunting, killing, laughing, living?');
+    while (array = 'laughing') {
+    if (answer !== "laughing") {
+        alert('It is not' + ' ' + answer + '!');
+        var answer = prompt('What is my favorite activity?' + 'Hunting, killing, laughing, living?');}
+    else (answer == "laughing");{
     }
 }
+    alert("Yeah! I love laughing!");
 }
-question()
+question();
